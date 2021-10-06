e0a97d817a2a6527202415d2309eb3038f1b178c
Stefan Bender <stefan.bender@ntnu.no>
2021-10-06 11:02:40 +0200

    tests: Text general-purpose spectra integration
    Tests the spectra integration, for now only that it runs successfully,
    and that it produces correctly-shaped result arrays.

--- tests/test_spectra.py
+++ tests/test_spectra.py
@@ -3,6 +3,7 @@ import numpy as np
 import pytest
 
 import eppaurora.spectra as spec
+from eppaurora.electrons import fang2010_mono
 
 # unit particle flux
 PFLUX_NNORM = [
@@ -40,4 +41,40 @@ def test_pflux_norm(pflux_func):
 	dfluxes = pflux_func(energies)
 	norm = np.trapz(dfluxes * energies, energies)
 	np.testing.assert_allclose(norm, 1., rtol=1e-3)
+	return
+
+
+@pytest.mark.parametrize(
+	"pflux_func",
+	PFLUX_ENORM + [spec.pflux_pow],
+)
+def test_ediss_spec_int(pflux_func):
+	energies = np.logspace(-2, 4, 257)
+	dfluxes = pflux_func(energies)
+	ediss = spec.ediss_spec_int(
+		energies, dfluxes,
+		6e5, 5e-10,
+		fang2010_mono,
+	)
+	assert ediss.shape == (1, 1)
+	return
+
+
+@pytest.mark.parametrize(
+	"pflux_func",
+	PFLUX_ENORM + [spec.pflux_pow],
+)
+def test_ediss_specfun_int(pflux_func):
+	energies = np.logspace(-1, 2, 4)
+	fluxes = 1
+	# ca. 100 km
+	scale_height = 6e5
+	rho = 5e-10
+	ediss = spec.ediss_specfun_int(
+		energies, fluxes,
+		scale_height, rho,
+		fang2010_mono,
+		spec_fun=pflux_func,
+	)
+	assert ediss.shape == (1, 4)
 	return
