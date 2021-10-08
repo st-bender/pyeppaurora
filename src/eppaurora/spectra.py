6a2ce8b881599fb4cc55caa6323ded5c3bd9ad79
Stefan Bender <stefan.bender@ntnu.no>
2021-10-08 13:19:42 +0200

    docs: Update in-line documentation for the spectra
    Includes equations and references for the different spectra that are
    typically used, as well as small typo fixes.
    
    [skip ci]

--- src/eppaurora/spectra.py
+++ src/eppaurora/spectra.py
@@ -30,9 +30,13 @@ __all__ = [
 
 # General normalized spectra, standard distributions
 def exp_general(en, en_0=10.):
-	r"""Exponential number flux spectrum as in Aksnes et al., 2006 [0]
+	r"""Exponential number flux spectrum
 
-	Defined according to Aksnes et al., JGR 2006, Eq. (1),
+	.. math::
+		\phi(E, E_0) = 1 / E_0 \cdot \exp\{-E / E_0\}
+
+	Standard exponential distribution with
+	:math:`\lambda` = 1 / ``en_0`` or :math:`\beta` = ``en_0``.
 	normalized to unit number flux, i.e.
 	:math:`\int_0^\infty \phi(E) \text{d}E = 1`.
 
@@ -48,19 +52,23 @@ def exp_general(en, en_0=10.):
 	-------
 	phi: float or array_like (N,)
 		Normalized differential hemispherical number flux at `en` in [keV-1 cm-2 s-1]
-		([keV] or scaled by 1 keV-2 cm-2 s-1, e.g. ).
+		([keV] or scaled by 1 keV-2 cm-2 s-1, e.g.).
 	"""
 	return 1. / en_0 * np.exp(-en / en_0)
 
 
 def gaussian_general(en, en_0=10., w=1.):
-	r"""Gaussian number flux spectrum as in Fang2008 [1]
+	r"""Gaussian number flux spectrum
 
-	Standard normal distribution with mu = en_0 and sigma = w / sqrt(2)
-	for use in Fang et al., JGR 2008, Eq. (1).
-	Almost normalized to unit number flux, i.e.
+	Standard normal distribution with
+	:math:`\mu` = ``en_0`` and :math:`\sigma` = ``w`` / sqrt(2):
+
+	.. math::
+		\phi(E, E_0, W) = 1 / \sqrt{\pi}W \cdot \exp\{-(E - E_0)^2 / W^2\}
+
+	Almost normalized to unit number flux
 	:math:`\int_0^\infty \phi(E) \text{d}E = 1`
-	(ignoring the negative tail).
+	(ignoring the negative tail for large ``en_0`` / ``w`` ratios).
 
 	Parameters
 	----------
@@ -71,6 +79,7 @@ def gaussian_general(en, en_0=10., w=1.):
 		Default: 10 keV
 	w: float, optional
 		Width of the Gaussian distribution, in [keV].
+		Default: 1 keV
 
 	Returns
 	-------
@@ -82,10 +91,16 @@ def gaussian_general(en, en_0=10., w=1.):
 
 
 def maxwell_general(en, en_0=10.):
-	r"""Maxwell number flux spectrum as in Fang2008 [1]
+	r"""Maxwell number flux spectrum
 
-	Defined in Fang et al., JGR 2008, Eq. (1),
-	normalized to :math:`\int_0^\infty \phi(E) \text{d}E = 1`.
+	.. math::
+		\phi(E, E_0) = E / E_0^2 \cdot \exp\{-E / E_0\}
+
+	Equal to a standard Gamma distribution with
+	:math:`\alpha` = 2 and :math:`\beta` = 1 / ``en_0``,
+	or
+	:math:`k` = 2 and :math:`\theta` = ``en_0``.
+	Normalized to :math:`\int_0^\infty \phi(E) \text{d}E = 1`.
 
 	Parameters
 	----------
@@ -105,9 +120,22 @@ def maxwell_general(en, en_0=10.):
 
 
 def pow_general(en, en_0=10., gamma=-3., het=True):
-	r"""Power-law number flux spectrum as in Strickland1993 [3]
+	r"""Power-law number flux spectrum
 
-	Defined e.g. in Strickland et al., 1993,
+	.. math::
+		\phi(E, E_0, \gamma) = \mp (\gamma + 1) / E_0 \cdot (E / E_0)^\gamma
+
+	The minus-sign (-) and is used for the high-energy tail variant,
+	and the plus-sign (+) for the low-energy tail variant.
+	The exponent ``gamma`` needs to be set appropriately,
+	< -1 for `het`, and > 1 for `let`.
+
+	The "high-energy tail" version (`het` = `True`)
+	resembles a Pareto distribution with
+	scale parameter :math:`x_m` = ``en_0``
+	and shape parameter :math:`\alpha` = -(``gamma`` + 1).
+
+	Adapted from Strickland et al., 1993 [#]_ and
 	normalized to unit particle flux:
 	:math:`\int_{E_0}^\infty \phi(E) \text{d}E = 1`
 	for the high-energy tail version, and
@@ -133,6 +161,12 @@ def pow_general(en, en_0=10., gamma=-3., het=True):
 	phi: float or array_like (N,)
 		Normalized differential hemispherical number flux at `en` in [keV-1 cm-2 s-1]
 		([keV] or scaled by 1 keV-2 cm-2 s-1, e.g.).
+
+	References
+	----------
+	.. [#] D. J. Strickland, R. E. Daniell Jr, J. R. Jasperse, B. Basu
+		J. Geophys. Res., 98(A12), pp. 21533--21548, 1993
+		doi: `10.1029/93JA01645 <https://doi.org/10.1029/93JA01645>`_
 	"""
 	spec = (gamma + 1) / en_0 * (en / en_0)**gamma
 	if het:
@@ -145,8 +179,14 @@ def pow_general(en, en_0=10., gamma=-3., het=True):
 def pflux_exp(en, en_0=10.):
 	r"""Exponential particle flux spectrum
 
+	.. math::
+		\phi(E, E_0) = 1 / E_0^2 \cdot \exp\{-E / E_0\}
+
 	Normalized to unit energy flux:
 	:math:`\int_0^\infty \phi(E) E \text{d}E = 1`.
+
+	Scales to arbitrary energy flux :math:`Q` via multiplication:
+	:math:`\tilde\phi = Q \cdot \phi`.
 
 	Parameters
 	----------
@@ -160,7 +200,11 @@ def pflux_exp(en, en_0=10.):
 	-------
 	phi: float or array_like (N,)
 		Hemispherical differential particle flux at `en` in [keV-1 cm-2 s-1]
-		([kev-2] scaled by unit energy flux).
+		([keV-2] scaled by unit energy flux).
+
+	See Also
+	--------
+	exp_general
 	"""
 	return exp_general(en, en_0=en_0) / en_0
 
@@ -168,10 +212,17 @@ def pflux_exp(en, en_0=10.):
 def pflux_gaussian(en, en_0=10., w=1):
 	r"""Gaussian particle flux spectrum
 
-	Defined in Fang et al., JGR 2008, Eq. (1).
+	As used in, e.g., Strickland et al., 1993 [#]_
 
-	Normalized to :math:`\int_0^\infty \phi(E) E \text{d}E = 1`.
+	.. math::
+		\phi(E, E_0, W) = 1 / \sqrt{\pi}E_0W \cdot \exp\{-(E - E_0)^2 / W^2\}
+
+	Normalized to :math:`\int_0^\infty \phi(E) E \text{d}E = 1`
 	(ignoring the negative tail).
+
+	Scales to arbitrary energy flux :math:`Q` via multiplication:
+	:math:`\tilde\phi = Q \cdot \phi`.
+
 	Parameters
 	----------
 	en: float or array_like (N,)
@@ -185,18 +236,39 @@ def pflux_gaussian(en, en_0=10., w=1):
 	phi: float or array_like (N,)
 		Hemispherical differential particle flux at `en` in [keV-1 cm-2 s-1]
 		([kev-2] scaled by unit energy flux).
+
+	References
+	----------
+	.. [#] D. J. Strickland, R. E. Daniell, J. R. Jasperse, B. Basu
+		J. Geophys. Res., 98(A12), pp. 21533--21548, 1993
+		doi: 10.1029/93JA01645
+
+	See Also
+	--------
+	gaussian_general
 	"""
 	return gaussian_general(en, en_0=en_0, w=w) / en_0
 
 
 def pflux_maxwell(en, en_0=10.):
-	r"""Maxwell particle flux spectrum as in Fang2008 [1]
+	r"""Maxwell particle flux spectrum
 
-	Defined in Fang et al., JGR 2008, Eq. (1).
+	As used in, e.g., Strickland et al., 1993 [#]_
+
+	.. math::
+		\phi(E, E_0) = E / 2E_0^3 \cdot \exp\{-E / E_0\}
+
+	Equal to a standard Gamma distribution with
+	:math:`\alpha` = 3 and :math:`\beta` = 1 / ``en_0``,
+	or
+	:math:`k` = 3 and :math:`\theta` = ``en_0``.
 	The total precipitating energy flux is fixed to 1 keV cm-2 s-1,
 	multiply by Q_0 [keV cm-2 s-1] to scale the particle flux.
 
 	Normalized to :math:`\int_0^\infty \phi(E) E \text{d}E = 1`.
+
+	Scales to arbitrary energy flux :math:`Q` via multiplication:
+	:math:`\tilde\phi = Q \cdot \phi`.
 
 	Parameters
 	----------
@@ -211,6 +283,16 @@ def pflux_maxwell(en, en_0=10.):
 	phi: float or array_like (N,)
 		Hemispherical differential particle flux at `en` in [keV-1 cm-2 s-1]
 		([kev-2] scaled by unit energy flux).
+
+	References
+	----------
+	.. [#] D. J. Strickland, R. E. Daniell, J. R. Jasperse, B. Basu
+		J. Geophys. Res., 98(A12), pp. 21533--21548, 1993
+		doi: 10.1029/93JA01645
+
+	See Also
+	--------
+	maxwell_general
 	"""
 	return 0.5 / en_0 * maxwell_general(en, en_0)
 
@@ -218,11 +300,23 @@ def pflux_maxwell(en, en_0=10.):
 def pflux_pow(en, en_0=10., gamma=-3., het=True):
 	r"""Power-law particle flux spectrum
 
-	Defined e.g. in Strickland et al., 1993.
+	As used in, e.g., Strickland et al., 1993 [#]_
+
+	.. math::
+		\phi(E, E_0, \gamma) = \mp (\gamma + 2) / E_0^2 \cdot (E / E_0)^\gamma
+
+	The minus-sign (-) and is used for the high-energy tail variant,
+	and the plus-sign (+) for the low-energy tail variant.
+	The exponent ``gamma`` needs to be set appropriately,
+	< -1 for `het`, and > 1 for `let`.
+
 	Normalized to :math:`\int_{E_0}^\infty \phi(E) E \text{d}E = 1`
 	for the high-energy tail version, and to
 	:math:`\int_0^{E_0} \phi(E) E \text{d}E = 1`
 	for the low-energy tail version.
+
+	Scales to arbitrary energy flux :math:`Q` via multiplication:
+	:math:`\tilde\phi = Q \cdot \phi`.
 
 	Parameters
 	----------
@@ -242,7 +336,17 @@ def pflux_pow(en, en_0=10., gamma=-3., het=True):
 	-------
 	phi: float or array_like (N,)
 		Hemispherical differential particle flux at `en` in [keV-1 cm-2 s-1]
-		([kev-2] scaled by unit energy flux).
+		([keV-2] scaled by unit energy flux).
+
+	References
+	----------
+	.. [#] D. J. Strickland, R. E. Daniell Jr, J. R. Jasperse, B. Basu
+		J. Geophys. Res., 98(A12), pp. 21533--21548, 1993
+		doi: `10.1029/93JA01645 <https://doi.org/10.1029/93JA01645>`_
+
+	See Also
+	--------
+	pow_general
 	"""
 	return (gamma + 2) / (gamma + 1) / en_0 * pow_general(en, en_0=en_0, gamma=gamma, het=het)
 
@@ -349,6 +453,7 @@ def ediss_specfun_int(
 		Number of integration steps, default: 128.
 	spec_func: callable, optional, default :func:`pflux_maxwell`
 		Spectral shape function, choices are:
+
 		* :func:`pflux_exp` for a exponential spectrum
 		* :func:`pflux_gaussian` for a Gaussian shaped spectrum
 		* :func:`pflux_maxwell` for a Maxwellian shaped spectrum
